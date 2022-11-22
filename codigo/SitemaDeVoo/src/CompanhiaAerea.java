5ca4e8c96d33f105ef0cd756224c95a06a26d966
HELLEN ROMANHOL <HELLEN@ONFLY.com>
2022-11-22 15:23:16 -0300

    add classe company aerea e implementação de metodos

---
+++ codigo/SitemaDeVoo/src/CompanhiaAerea.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class CompanhiaAerea {
+    private ArrayList<Voo> voos = new ArrayList<>(200);
+    private ArrayList<Cliente> clientes = new ArrayList<>(200);
+
+    /**
+   * Cria uma compnhia aerea com um arraylist de clientes cadastrados e voos.
+   */
+  public CompanhiaAerea(){
+    this.voos= new ArrayList<>();
+    this.clientes = new ArrayList<>();
+  }
+
+  /**
+   * Adiciona novo voo na lista de voos da companhia
+   * @param novo
+   */
+  public void adicionarBilhete(Voo novo){
+    this.voos.add(novo);
+  }
+
+  /**
+   * Percorre a lista cliente, somando o valor que ele gastou na companhia
+   */
+  public double calcularTotalArrecado(){
+    double total = this.clientes.stream()
+                              .mapToDouble(Cliente::calcularTotalDosBilhetes)
+                              .sum();
+    return total;
+  }
+
+
+  /**
+   * Adiciona novo cliente na lista de clientes da companhia
+   * @param novo
+   */
+  public void adicionarClente(Cliente novo){
+    this.clientes.add(novo);
+  }
+}
